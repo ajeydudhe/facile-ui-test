1c61f36ddb89bcc2ed2be42ca931f535dc755b6f
Ajey Dudhe <ajeydudhe@gmail.com>
2019-10-20 01:06:40 +0530

    Using faclie-mock to mock http calls.

--- samples/spring-boot-web/src/test/java/org/expedientframework/uitest/HomePageTest.java
+++ samples/spring-boot-web/src/test/java/org/expedientframework/uitest/HomePageTest.java
@@ -2,9 +2,12 @@ package org.expedientframework.uitest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.expedientframework.facilemock.http.browsermob.HttpMockContext;
+import org.expedientframework.facilemock.http.browsermob.HttpProxyManagerFactory;
 import org.expedientframework.uitest.pages.HomePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -12,18 +15,39 @@ import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+import static org.expedientframework.facilemock.http.browsermob.HttpMockHelpers.*;
+
 public class HomePageTest {
   
   @Test
   public void applicationName_validName() {
     
-    this.webDriver = new ChromeDriver();
-    
-    this.webDriver.get("http://localhost:8080");
-    
-    final HomePage homePage = PageFactory.initElements(this.webDriver, HomePage.class);
-    
-    assertThat(homePage.getAppName()).as("Application Name").isEqualTo("Facile UI Test Web Application");    
+    try (HttpMockContext mock = HttpProxyManagerFactory.createMockContext(unitTest())) {
+      
+      final ChromeOptions chromeOptions = new ChromeOptions();
+      
+      chromeOptions.setHeadless(true);
+      
+      this.webDriver = new ChromeDriver(chromeOptions);
+      
+      mock.when(urlEquals("/")).then(respondWith("<html>\n" + 
+          "  <head>\n" + 
+          "    <title>Home Page</title>\n" + 
+          "  </head>\n" + 
+          "  <body>\n" + 
+          "    <h1>Hello !</h1>\n" + 
+          "    <p>\n" + 
+          "      Welcome to <b><span id=\"lblAppName\">Facile UI Test Web Application</span></b>\n" + 
+          "    </p>\n" + 
+          "  </body>\n" + 
+          "</html>"));
+
+      this.webDriver.get(String.format("http://localhost:%d", mock.getHttpProxyManager().getPort()));
+      
+      final HomePage homePage = PageFactory.initElements(this.webDriver, HomePage.class);
+      
+      assertThat(homePage.getAppName()).as("Application Name").isEqualTo("Facile UI Test Web Application");    
+    }    
   }
   
   @BeforeClass
